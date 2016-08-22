ab1ff6113dedbc2495134c9826b80be1c21cc946
Ilias Moreno Alui <ilias.moreno.alui.contractor@bbva.com>
2016-08-22 08:52:06 +0200

    fix(bower): Initial problems fixed

--- plugins/bower/index.js
+++ plugins/bower/index.js
@@ -1,35 +1,34 @@
 'use strict';
 
-function _bowerInstall(ok, ko) {
-  // bower install by default
-  this.logger.info('#white', 'bower install');
-  const args = [ 'install' ]
-    .concat(this.params.bower.dependencies);
-
-  // Save dependencies option
-  if (this.params.bower.save === 'save') {
-    args.push('--save');
-  } else if (this.params.bower.save === 'saveDev') {
-    args.push('--save-dev');
-  }
-
-  // Force Latest option
-  if (this.params.bower.forceLatest === 'true') {
-    args.push('--force-latest');
-  }
-
-  // Offline option
-  if (this.params.bower.offline === 'true') {
-    args.push('--offline');
-  }
-  this.logger.info('#blue', 'bower args', args);
-  this.executeSync('bower', args, ko, true);
-}
-
 module.exports = {
   run: function(ok, ko) {
+    const _bowerInstall = (_ok, _ko) => {
+      // bower install by default
+      this.logger.info('#white', 'bower install');
+      let args = [ 'install' ];
+
+      // Save dependencies option
+      if (this.params.bower.save === 'save') {
+        args.push('--save');
+      } else if (this.params.bower.save === 'saveDev') {
+        args.push('--save-dev');
+      }
+
+      // Force Latest option
+      if (this.params.bower.forceLatest) {
+        args.push('--force-latest');
+      }
+
+      // Offline option
+      if (this.params.bower.offline) {
+        args.push('--offline');
+      }
+      this.logger.info('#blue', 'bower args', args);
+      this.executeSync('bower', args, _ko, true);
+    };
+
     if (this.params.bower.install) {
-      _bowerInstall(ok, ko);
+      _bowerInstall(ko, ok);
     }
   }
 };
