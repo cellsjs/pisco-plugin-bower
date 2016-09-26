840049cf8e51af2b89e84e81597b5e6044494162
sbonacho <sbonacho@gmail.com>
2016-09-26 18:06:56 +0200

    feat(): bower list + params

--- plugins/bower/index.js
+++ plugins/bower/index.js
@@ -2,57 +2,22 @@
 
 module.exports = {
 
-  check: function() {
+  check() {
     this.params.bower = this.params.bower ? this.params.bower : {};
     if (this.params.dependenciesInstalled) {
       return this.bowerInstall();
     }
   },
-
-  /*
-  run: function(ok, ko) {
-    let result;
-    const _executeBower = (command, _ok, _ko) => {
-      // bower install by default
-      this.logger.info('#white', `bower ${command}`);
-      let args = [ command ];
-
-      // Save dependencies option
-      if (this.params.bower.save === 'save') {
-        args.push('--save');
-      } else if (this.params.bower.save === 'saveDev') {
-        args.push('--save-dev');
-      }
-
-      // Force Latest option
-      if (this.params.bower.forceLatest) {
-        args.push('--force-latest');
-      }
-
-      // Offline option
-      if (this.params.bower.offline) {
-        args.push('--offline');
-      }
-
-      // json option
-      if (this.params.bower.json) {
-        args.push('--json');
-      }
-
-      this.logger.info('#blue', 'bower args', args);
-      return this.executeSync('bower', args, _ko, false);
-    };
-
-    if (this.params.bower.install) {
-      result = _executeBower('install', ko, ok);
-    } else if (this.params.bower.list) {
-      result = _executeBower('list', ko, ok);
-    }
-
-    this.params.bower.result = result.stdout.toString();
-  },
-*/
   addons: {
+    bowerList(params) {
+      this._bowerPre();
+      let args = [ 'list' ];
+      if (params) {
+        args = args.concat(args, params);
+      }
+      return this.execute('bower', args)
+        .then(() => this._bowerPost());
+    },
     bowerIsBaseDir() {
       return this.params.bower.baseDir && this.fsExists(this.params.bower.baseDir);
     },
