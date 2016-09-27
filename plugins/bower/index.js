d095c426595fd3409f755a881ef42f1aad11c033
sbonacho <sbonacho@gmail.com>
2016-09-27 14:07:50 +0200

    feat(): private method ordered.

--- plugins/bower/index.js
+++ plugins/bower/index.js
@@ -8,10 +8,46 @@ module.exports = {
   check() {
     this.params.bower = this.params.bower ? this.params.bower : {};
     if (this.params.bower.installed || this.params.bower.updated) {
-      return this.bowerInstall();
+      return this._bowerAction();
     }
   },
   addons: {
+    bowerList(params) {
+      this._bowerPre();
+      let args = [ 'list' ];
+      if (params) {
+        args = args.concat(args, params);
+      }
+      return this.execute('bower', args)
+        .then((result) => this._bowerPost(result));
+    },
+    bowerDirectory() {
+      return this.params.bower.directory ? this.params.bower.directory : 'bower_components';
+    },
+    bowerIsInstalled() {
+      this._bowerPre();
+      const result = this.fsExists(this.bowerDirectory());
+      this._bowerPost();
+      return result;
+    },
+    _bowerAction() {
+      if (!this.bowerIsInstalled()) {
+        this.logger.info('Bower', '#green', 'installing', '...');
+        return this._bowerInstall();
+      } else if (this.params.bower.updated) {
+        this.logger.info('Bower', '#green', 'updating', '...');
+        return this._bowerAreSymbolicLinks()
+          .then((result) => this._bowerCheckIfUpdate(result));
+      } else {
+        this.logger.info('Bower installed:', '#green', 'OK');
+      }
+    },
+    _bowerInstall() {
+      this._bowerPre();
+      const args = this._bowerForceLatest([ 'install' ]);
+      return this.execute('bower', args)
+        .then(() => this._bowerPost());
+    },
     _bowerAreSymbolicLinks() {
       this._bowerPre();
       if (!this.bowerIsInstalled()) {
@@ -43,25 +79,6 @@ module.exports = {
         });
       });
     },
-    _bowerPre() {
-      if (this.bowerIsBaseDir()) {
-        process.cwd(this.params.bower.baseDir);
-      }
-    },
-    _bowerPost(result) {
-      if (this.bowerIsBaseDir()) {
-        process.cwd(this.params.workingDir);
-      }
-      return result;
-    },
-    _bowerPromptLinks() {
-      this.params.promptLinks = [ {
-        type: 'confirm',
-        name: 'doBowerUpdate',
-        required: true,
-        message: `There are symbolic links in your ${this.bowerDirectory()} folder. Do you want to update bower dependencies anyway?`
-      } ];
-    },
     _bowerCheckIfUpdate(check) {
       if (check) {
         this._bowerPromptLinks();
@@ -80,11 +97,24 @@ module.exports = {
       }
       return args;
     },
-    _bowerInstall() {
-      this._bowerPre();
-      const args = this._bowerForceLatest([ 'install' ]);
-      return this.execute('bower', args)
-        .then(() => this._bowerPost());
+    _bowerPre() {
+      if (this._bowerIsBaseDir()) {
+        process.cwd(this.params.bower.baseDir);
+      }
+    },
+    _bowerPost(result) {
+      if (this._bowerIsBaseDir()) {
+        process.cwd(this.params.workingDir);
+      }
+      return result;
+    },
+    _bowerPromptLinks() {
+      this.params.promptLinks = [ {
+        type: 'confirm',
+        name: 'doBowerUpdate',
+        required: true,
+        message: `There are symbolic links in your ${this.bowerDirectory()} folder. Do you want to update bower dependencies anyway?`
+      } ];
     },
     _bowerUpdate() {
       this._bowerPre();
@@ -93,39 +123,8 @@ module.exports = {
         .then(() => this.execute('bower', [ 'prune' ]))
         .then(() => this._bowerPost());
     },
-    bowerList(params) {
-      this._bowerPre();
-      let args = [ 'list' ];
-      if (params) {
-        args = args.concat(args, params);
-      }
-      return this.execute('bower', args)
-        .then((result) => this._bowerPost(result));
-    },
-    bowerIsBaseDir() {
+    _bowerIsBaseDir() {
       return this.params.bower.baseDir && this.fsExists(this.params.bower.baseDir);
-    },
-    bowerDirectory() {
-      return this.params.bower.directory ? this.params.bower.directory : 'bower_components';
-    },
-    bowerIsInstalled() {
-      this._bowerPre();
-      const result = this.fsExists(this.bowerDirectory());
-      this._bowerPost();
-      return result;
-    },
-    bowerInstall() {
-      if (!this.bowerIsInstalled()) {
-        this.logger.info('Bower', '#green', 'installing', '...');
-        return this._bowerInstall();
-      } else if (this.params.bower.updated) {
-        this.logger.info('Bower', '#green', 'updating', '...');
-        return this._bowerAreSymbolicLinks()
-          .then((result) => this._bowerCheckIfUpdate(result));
-      } else {
-        this.logger.info('Bower installed:', '#green', 'OK');
-      }
     }
   }
-
 };
