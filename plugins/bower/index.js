95fd3ad835428019d07a005de164933d5b14b948
sbonacho <sbonacho@gmail.com>
2017-02-23 15:17:30 +0100

    feat(): bowerShrimwrap and lockedInstal parameter to make wrap the installation, bowerProduction parameter

--- plugins/bower/index.js
+++ plugins/bower/index.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const path = require('path');
+const glob = require('glob');
+const shrinkwrapFile = 'bower_shrinkwrap.json';
 
 module.exports = {
 
@@ -24,6 +26,50 @@ module.exports = {
   },
 
   addons: {
+    bowerConsolidate(config) {
+      return this._bowerAction()
+        .then(() => this._bowerCompare(config));
+    },
+    _bowerCompare(config) {
+      const file = shrinkwrapFile;
+      this._bowerPre();
+      return this.bowerShrinkwrap()
+        .then(() => this._bowerPost())
+        .then(() => {
+          const oldf = this.fsReadFile(`_${file}`);
+          const newf = this.fsReadFile(file);
+          fs.renameSync(file, `_${file}`);
+          config.hasChanged = oldf.toString() !== newf.toString() ? `_${file}` : false;
+          this.logger.info('Dependencies has changed', config.hasChanged ? '#green' : '#yellow', config.hasChanged !== false);
+          return config;
+        });
+    },
+    _bowerReadDep(file, dependencies) {
+      const json = this.fsReadConfig(file);
+      if (json._originalSource || json.version || json._resolution && json._resolution.commit) {
+        json.version = json.version ? json.version : json._resolution.commit;
+        dependencies[json.name] = json._originalSource === json.name ? json.version : `${json._originalSource}#${json.version}`;
+      } else {
+        return Promise.reject({error: `Dependency "${json.name}" has no version or commit: Is not possible to be Shrink-wrapped`});
+      }
+    },
+    bowerShrinkwrap() {
+      const promises = [];
+      const dependencies = {};
+      return new Promise((ok, ko) => {
+        glob(`${this.bowerDirectory()}/**/.bower.json`, {}, (er, files) => {
+          files.forEach((file) => {
+            promises.push(this._bowerReadDep(file, dependencies));
+          });
+          Promise.all(promises).then(ok).catch(ko);
+        });
+      })
+      .then(() => {
+        const bower = this.fsReadConfig('bower.json');
+        bower.dependencies = dependencies;
+        fs.writeFileSync(shrinkwrapFile, JSON.stringify(bower, null, 2));
+      });
+    },
     bowerList(params) {
       this._bowerPre();
       let args = [ 'list' ];
@@ -55,11 +101,12 @@ module.exports = {
           .then((result) => this._bowerCheckIfUpdate(result));
       } else {
         this.logger.info('Bower installed:', '#green', 'OK');
+        return Promise.resolve();
       }
     },
     _bowerInstall() {
       this._bowerPre();
-      const args = this._bowerForceLatest([ 'install' ]);
+      const args = this._bowerForceLatest(this._bowerProduction([ 'install' ]));
       return this.execute('bower', args)
         .then(() => this._bowerPost());
     },
@@ -112,15 +159,29 @@ module.exports = {
       }
       return args;
     },
+    _bowerProduction(args) {
+      if (this.params.bower.production !== false) {
+        args.push('-p');
+      }
+      return args;
+    },
     _bowerPre() {
       if (this._bowerIsBaseDir()) {
         this.params._origAbsDir = process.cwd();
         process.chdir(this.params.bower.baseDir);
       }
+      if (this.params.bower.lockedInstall && this.fsExists(`_${shrinkwrapFile}`)) {
+        fs.renameSync('bower.json', '_bower.json');
+        fs.renameSync(`_${shrinkwrapFile}`, 'bower.json');
+      }
     },
     _bowerPost(result) {
       if (this._bowerIsBaseDir()) {
         process.chdir(this.params._origAbsDir);
+      }
+      if (this.params.bower.lockedInstall && this.fsExists('_bower.json')) {
+        fs.renameSync('bower.json', `_${shrinkwrapFile}`);
+        fs.renameSync('_bower.json', 'bower.json');
       }
       return result;
     },
